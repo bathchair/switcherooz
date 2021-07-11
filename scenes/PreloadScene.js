30aa0b656db83192cad2ab405ad099c00d92e3a6
Dea Harjianto <64983004+bathchair@users.noreply.github.com>
2021-07-11 01:10:10 -0400

    style: new buttons & hover

--- scenes/PreloadScene.js
+++ scenes/PreloadScene.js
@@ -5,11 +5,20 @@ export default class PreloadScene extends Phaser.Scene {
     }
 
     preload() {
+        // background image for title screen
         this.load.image("background", "assets/images/background.png")
         this.load.image("background_1", "assets/images/background_1.png")
         this.load.image("background_2", "assets/images/background_2.png")
         this.load.image("background_3", "assets/images/background_3.png")
         this.load.image("background_4", "assets/images/background_4.png")
+
+        // title screen buttons
+        this.load.image("play", "assets/images/Play Button.png")
+        this.load.image("options", "assets/images/Options Button.png")
+        this.load.image("controls", "assets/images/Controls Button.png")
+
+        // title gfx
+        this.load.image("title_v1", "assets/images/title_v1.png")
     }
 
     create() {
--- scenes/TitleScene.js
+++ scenes/TitleScene.js
@@ -13,11 +13,27 @@ export default class TitleScene extends Phaser.Scene {
             this.add.tileSprite(0,0,this.game.config.width,this.game.config.height,"background_4"),
         ]
 
+        this.menuButtons = [
+            this.playButton = this.add.image(this.game.config.width / 2, 300, "play").setScale(0.4).setInteractive({useHandCursor:true}),
+            this.controlsButton = this.add.image(this.game.config.width / 2, 400, "controls").setScale(0.4).setInteractive({useHandCursor:true}),
+            this.optionsButton = this.add.image(this.game.config.width / 2, 500, "options").setScale(0.4).setInteractive({useHandCursor:true})
+        ]
+
+        this.title = this.add.text(this.game.config.width / 20, 50, "STAR SWITCH").setStyle({fontFamily: 'Londrina Solid'})
+        this.title.setOrigin(0,0)
+
         this.changeSpeed = [ 0.5, 0.3, 0.7, 0.2 ]
 
         this.background.forEach(function(item) {
             item.setOrigin(0,0)
-        });
+        })
+
+        this.menuButtons.forEach(function(item) {
+            item.on('pointerover', function() {item.setTint(0xf2cd60)})
+            item.on('pointerout', function() {item.setTint(0xffffff)})
+            item.on('pointerdown', function() {item.setTint(0xbd910f)})
+            item.on('pointerup', function() {item.setTint(0xf2cd60)})
+        })
     }
 
     update() {
